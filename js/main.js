c29ba18561711e3ad852168859410d37568cdf13
floating-byte <79982823+floating-byte@users.noreply.github.com>
2021-06-07 08:14:09 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,266 @@
+$(function(){
+  loadNotes();
+  expandWindow();
+
+  save_A_Note();
+  remove_A_Note();
+
+  clearForm();
+  editTitle();
+}());
+function noteAction(){
+  showNoteInfo();
+  selecetNotes();
+}
+
+function setNotes(notes){
+  store.set("notes",notes);
+  loadNotes();
+}
+function getNote(id){
+  return store.get("notes")[parseInt(id)];
+}
+
+
+
+
+
+var open = false;
+function expandWindow(){
+  $("#create-note-btn").click(function(){
+    if(!open){
+      open = true;
+      $("#create-note-btn").text("Cancle");
+      $("#second-page").addClass("for-new-note");
+
+      if($(".for-new-note").legth == 0){
+        $("#second-page").addClass("for-new-note");
+      }
+
+      if($(".for-show-note").length != 0){
+        $("#second-page").removeClass("for-show-note");
+      }
+
+      renderSecendPage();
+      clearForm(true);
+    }
+    else{
+      open = false;
+      $("#create-note-btn").text("New Note");
+
+      if($(".for-new-note").length != 0){
+        $("#second-page").removeClass("for-new-note");
+      }
+
+
+      hideSecondPage();
+    }
+  });
+}
+
+function loadNotes(){
+  $("#first-page").empty();
+
+  let notes = store.get("notes");
+  if(notes == undefined){
+    return;
+  }
+
+  renderAllNotes(notes);
+  noteAction();
+}
+function clearForm(shouldClear){
+  function clear(){
+    $("#title-text").val("");
+    $("#desc-text").text("");
+    $("#new-comment-text").text("");
+    $("#comment-container").empty();
+    $("#add-comment-active").attr("id","add-comment-not-active");
+  }
+  if(shouldClear){
+    clear();
+    return ;
+  }
+  $("#cancel-btn").click(function(){
+    clearForm(true)
+  });
+}
+function save_A_Note(){
+  function getTheNote(){
+    let note = {};
+    note.title = $("#title-text").val();
+
+    if(note.title == ""){
+      $("#title-text").focus();
+      return undefined;
+    }
+    console.log("emp")
+
+    note.desc = $("#desc-text").text();
+    note.date = {};
+
+    let d = new Date();
+    note.date.year = getFullYear(d);
+    note.date.time = getFullTime(d);
+
+    note.comments = [];
+
+    let commTag = $(".comment-data");
+
+    for(let i = 0; i < commTag.length; i++){
+      let comment = {};
+      comment.text = $($(commTag[i]).find(".comment-text")).text();
+      if(comment.text != ""){
+        comment.date = {};
+        comment.date.year = $($(commTag[i]).find(".comment-date")).text();
+        comment.date.time = $($(commTag[i]).find(".comment-time")).text();
+
+        note.comments.push(comment);
+      }
+    }
+    return note;
+  }
+  $("#save-btn").click(function(){
+
+    let note = getTheNote();
+    if(note == undefined){
+      return;
+    }
+
+    let notes = [];
+    if(store.get("notes") != undefined){
+      notes = store.get("notes");
+    }
+
+    if($(".for-new-note").length != 0){
+      note.id = notes.length;
+      notes.push(note);
+    }
+    else{
+      let id = $("#note-id").text();
+      note.id = id;
+      notes[parseInt(id)] =  note;
+    }
+
+    setNotes(notes);
+    });
+}
+function remove_A_Note(id){
+  function remove(id){
+    let notes = store.get("notes");
+    notes.splice(parseInt(id),1);
+    for(let i = 0; i < notes.length;i++){
+      notes[i].id = i;
+    }
+
+    if($(".for-show-note").length != 0){
+      let shownNoteId = $(".for-show-note #note-id").text();
+      if(parseInt(id) == parseInt(shownNoteId)){
+        hideSecondPage();
+      }
+    }
+    setNotes(notes);
+  }
+
+  if(id != undefined){
+    remove(id);
+    return;
+  }
+  $("#delete-note").click(function(){
+      id = $("#option-note-id").text();
+      $("#note-option-menu").css("display","none");
+      remove_A_Note(id);
+    });
+}
+
+
+
+function editTitle(){
+  $("#edit-note").click(function(){
+
+    if($(".edit-box").length != 0){
+      $("#note-option-menu").css("display","none");
+      return ;
+    }
+    let tag;
+    tag = "<button type='button' id='save-edit-title'><img src='icons/save.svg'></button>"
+    $("#optimize-note").prepend(tag);
+    tag = '<input class="edit-box" spellcheck="false">';
+    $("#optimize-note").prepend(tag);
+
+    $("#optimize-note .date-info").hide();
+    $("#optimize-note .icons").hide();
+
+
+    let id = $("#option-note-id").text();
+    $(".edit-box").val(getNote(id).title);
+
+    $("#optimize-note .title").hide();
+    //$("#optimize-note").css("height","50px");
+    $("#note-option-menu").css("display","none");
+
+    saveEditedTitle();
+
+  });
+
+}
+function saveEditedTitle(){
+  $("#save-edit-title").click(function(){
+    let newTitle = $(".edit-box").val();
+    let parent = $($("#save-edit-title").parent());
+
+    if(newTitle == ""){return ;}
+    if(newTitle == parent.find(".title").text()){unSelecte(); return;}
+
+    let id = parent.find(".id").text();
+    let notes = store.get("notes");
+    notes[parseInt(id)].title = newTitle;
+
+    setNotes(notes);
+  });
+}
+
+
+
+function showNoteInfo(){
+  $(".note").click(function(){
+    if($(".edit-box").length != 0){
+      return;
+    }
+
+    renderSecendPage();
+    open = false;
+
+
+    if($(".for-show-note").length == 0){
+      $("#second-page").addClass("for-show-note");
+    }
+
+    if($(".for-new-note").length > 0){
+      $("#second-page").removeClass("for-new-note");
+      $("#create-note-btn").text("New Note");
+    }
+
+    let id = $(this).find(".id").text();
+    note = getNote(id);
+
+    $("#note-id").text(id);
+
+    $("#title-text").val(note.title);
+    $("#desc-text").text(note.desc);
+
+    $("#comment-container").empty();
+    for(let i = 0; i < note.comments.length;i++){
+      let tag =
+      `<div class="comment-data">
+        <div class="comment-date-info">
+        <p class="comment-date">${note.comments[i].date.year}</p>
+        <p class="comment-time">${note.comments[i].date.time}</p>
+        </div>
+        <p class="comment-text">${note.comments[i].text}<p>
+        </div>`;
+
+      $("#comment-container").append(tag);
+    }
+  });
+}
---
+++ js/pageAction.js
@@ -0,0 +1,190 @@
+$(function(){
+  commantHandler();
+  selecetNotes();
+  dropDownBtn();
+});
+
+function commantHandler(){
+  function closeNewCommentWindow(){
+    $("#new-comment-text").text("");
+    $("#add-comment-active").attr("id","add-comment-not-active");
+  }
+
+  $(document).on("mouseup",function(){
+    if($("#new-comment-text").is(":focus")){
+      if($("#add-comment-not-active").length != 0){
+        $("#add-comment-not-active").attr("id","add-comment-active");
+      }
+    }
+    else{
+      if($("#new-comment-text").text() == ""){
+        if($("#add-comment-active").lenght != 0){
+          closeNewCommentWindow();
+        }
+      }
+    }
+  });
+
+  $("#save-comment").click(function(){
+    let textVal = $("#new-comment-text").text();
+    textVal.replaceAll("/t","")
+
+    if(textVal != ""){
+      let d = new Date();
+
+      let tag =
+      `<div class="comment-data">
+        <div class="comment-date-info">
+        <p class="comment-date">${getFullYear(d)}</p>
+        <p class="comment-time">${getFullTime(d)}</p>
+        </div>
+        <p class="comment-text">${textVal}<p>
+        </div>`;
+      $("#comment-container").prepend(tag);
+      closeNewCommentWindow();
+    }
+  });
+
+}
+function selecetNotes(){
+   $(document).mousedown(function(){
+     if(!$(".note:hover").length != 0 && !$("#note-option-menu:hover").length != 0){
+
+       unSelecte();
+     }
+    });
+    $(".note").mousedown(function(event){
+      if(event.which == 3){
+        unSelecte();
+
+        $("#note-option-menu").css("display","block");
+        $("#note-option-menu").css("top",`${event.pageY}px`);
+        $("#note-option-menu").css("left",`${event.pageX}px`);
+
+        let id = $($(this).find(".id")).text();
+        $($("#note-option-menu").find("#option-note-id")).text(id);
+        $(this).attr("id","optimize-note");
+      }
+
+    });
+}
+
+
+function unSelecte(){
+  $("#note-option-menu").css("display","none");
+  $("#optimize-note .title").show();
+  //$("#optimize-note").css("height","50px");
+  $("#optimize-note .date-info").show();
+  $("#optimize-note .icons").show();
+  $("#optimize-note input").remove();
+  $("#optimize-note button").remove();
+  $("#optimize-note").removeAttr("id");
+}
+function renderAllNotes(notes){
+  for(let i = 0; i < notes.length; i++){
+    let descOpacity = 1;
+    if(notes[i].desc == ""){descOpacity = 0.5;}
+
+    let title = notes[i].title;
+    let maxChar = 14;
+    if(title.length > maxChar){
+      title = title.slice(0,maxChar)+"...";
+    }
+
+    let tag =
+    `
+    <div class="note"  oncontextmenu="return false;">
+    <p class="title">${title}</p>
+
+    <div class="date-info">
+    <p class="date">${notes[i].date.year}</p>
+    <p class="time">${notes[i].date.time}</p>
+    </div>
+
+    <div class="icons">
+
+    <img class="description-icon" src="icons/description.svg" style="opacity:${descOpacity}">
+    <div class="comment">
+    <p>${notes[i].comments.length}</p>
+    <img class="comment-icon" src="icons/comment.svg">
+    </div>
+
+    </div>
+    <div class="id" style="display:none">${notes[i].id}</div>
+    </div>
+    `
+    $("#first-page").prepend(tag);
+  }
+
+}
+function renderSecendPage(){
+  unSelecte();
+  $("html").css("width",`741px`);
+  $("#first-page").css("margin-left","400px");
+  $("#create-note-btn").css("left",`526px`);
+
+  $("#second-page").css("display","block");
+  $("#add-url").show();
+  $("#cancel-btn").show();
+  $("#save-btn").show();
+}
+function hideSecondPage(){
+  $("#add-url").hide();
+  $("#cancel-btn").hide();
+  $("#save-btn").hide();
+  $("#second-page").css("display","none");
+
+  $("html").css("width",`352px`);
+  $("#first-page").css("margin-left","459px");
+
+  $("#first-page").css("margin-left","11px");
+  $("#create-note-btn").css("left","133px");
+  $("#second-page").css("display","none");
+
+
+}
+
+
+function dropDownBtn(){
+  function selectDisplay(attr,rotate){
+    let currDisplay = $(`${attr}`).css("display");
+
+    let rotation = currDisplay == "none" ? "0":"180";
+    $(`${rotate}`).css("transform",`rotate(${rotation}deg)`);
+
+    return currDisplay == "none" ? "block" : "none";
+  }
+
+  $("#desc-dropdown").click(function(){
+    $("#desc-text").css("display",selectDisplay("#desc-text","#desc-dropdown"));
+  });
+  $("#comment-dropdown").click(function(){
+    $("#new-comment-text").css("display",selectDisplay("#new-comment-text","#comment-dropdown"));
+  });
+}
+
+
+function getFullYear(date){
+  let year = date.getFullYear();
+  let day = date.getDate() ;
+  let month = date.getMonth() +1;
+
+  day = addZero(day);
+  month = addZero(month);
+
+  return `${year}/${month}/${day}`;
+}
+function getFullTime(date){
+  let minute = date.getMinutes();
+  let hour = date.getHours();
+  minute = addZero(minute);
+  hour = addZero(hour);
+
+  return `${hour}:${minute}`;
+}
+function addZero(val){
+  if(val <  10){
+    return `0${val}`;
+  }
+  return `${val}`;
+}
